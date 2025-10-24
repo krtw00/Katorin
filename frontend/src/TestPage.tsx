3a0b3a6d33b9b62c854692aec86a1cb68c60a4e2
H.Iguchi <grandchariot001@gmail.com>
2025-10-25 00:19:18 +0900

    テストページをフロントエンドに追加

---
+++ frontend/src/TestPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TestPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Test Page</h1>
+      <p>This is a simple test page for the frontend.</p>
+      <p>If you see this, the frontend is working correctly!</p>
+    </div>
+  );
+};
+
+export default TestPage;
